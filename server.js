aa620391d79d81a39b2e5b71b0f44071b0783e97
Paul Morimoto <psmorimoto@gmail.com>
2019-01-24 20:36:57 -0800

    Create API for all services

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.get('/restaurants/:id', (req, res) => {
 
 app.get('/restaurants/:id/reviews', (req, res) => {
   // get all the reviews from the reviews server
-  httpReq.get(`http://ec2-54-183-88-68.us-west-1.compute.amazonaws.com:3005/restaurants/${req.params.id}/reviews`)
+  httpReq.get(`http://ec2-54-153-119-40.us-west-1.compute.amazonaws.com/restaurants/${req.params.id}/reviews`)
   .then(function (req) {
     res.status(200).send(req.data);
   })
@@ -24,65 +24,66 @@ app.get('/restaurants/:id/reviews', (req, res) => {
 
 });
 
-// app.get('/restaurants/:id/Suggestions', (req, res) => {
-//   // get all the Suggestions from the suggestions server
-//   httpReq.get(`http://ec2-54-183-207-43.us-west-1.compute.amazonaws.com/restaurants/${req.params.id}/Suggestions`)
-//   .then(function (req) {
-//     res.status(200).send(req.data);
-//   })
-//   .catch(function (error) {
-//     res.status(501).send(error);
-//   });
+app.get('/restaurants/:id/Suggestions', (req, res) => {
+  // get all the Suggestions from the suggestions server
+  httpReq.get(`http://18.188.10.73/restaurants/${req.params.id}/suggestions`)
+  .then(function (req) {
+    res.status(200).send(req.data);
+  })
+  .catch(function (error) {
+    res.status(501).send(error);
+  });
 
-// });
+});
 
-// app.get('/restaurants/:id/menu-items', (req, res) => {
-//   // get all the menu items from the menu server
-//   httpReq.get(`http://ec2-13-57-210-63.us-west-1.compute.amazonaws.com/restaurants/${req.params.id}/menu-items`)
-//   .then(function (req) {
-//     res.status(200).send(req.data);
-//   })
-//   .catch(function (error) {
-//     res.status(501).send(error);
-//   });
+app.get('/restaurants/:id/menu-items', (req, res) => {
+  // get all the menu items from the menu server
+  httpReq.get(`http://54.241.249.88/restaurants/${req.params.id}/menu-items`)
+  .then(function (req) {
+    res.status(200).send(req.data);
+  })
+  .catch(function (error) {
+    res.status(501).send(error);
+  });
 
-// });
+});
 
-// app.get('/restaurants/:id/menu-items/:itemId', (req, res) => {
-//   // get all the menu item Id from the menu server
-//   httpReq.get(`http://ec2-13-57-210-63.us-west-1.compute.amazonaws.com/restaurants/${req.params.id}/menu-items/${req.params.itemId}`)
-//   .then(function (req) {
-//     res.status(200).send(req.data);
-//   })
-//   .catch(function (error) {
-//     res.status(501).send(error);
-//   });
+app.get('/restaurants/:id/menu-items/:itemId', (req, res) => {
+  // get all the menu item Id from the menu server
+  httpReq.get(`http://54.241.249.88/restaurants/${req.params.id}/menu-items/${req.params.itemId}`)
+  .then(function (req) {
+    res.status(200).send(req.data);
+  })
+  .catch(function (error) {
+    res.status(501).send(error);
+  });
 
-// });
+});
 
-// app.get('/restaurants/:id/order', (req, res) => {
-//   // get all the menu order from the menu server
-//   httpReq.get(`http://ec2-13-57-210-63.us-west-1.compute.amazonaws.com/restaurants/${req.params.id}/order`)
-//   .then(function (req) {
-//     res.status(200).send(req.data);
-//   })
-//   .catch(function (error) {
-//     res.status(501).send(error);
-//   });
+app.get('/restaurants/:id/order', (req, res) => {
+  // get all the menu order from the menu server
+  httpReq.get(`http://54.241.249.88/restaurants/${req.params.id}/order`)
+  .then(function (req) {
+    res.status(200).send(req.data);
+  })
+  .catch(function (error) {
+    res.status(501).send(error);
+  });
 
-// });
+});
 
-// app.get('/restaurants/:id/profile', (req, res) => {
-//   // get all the profile from profile server
-//   httpReq.get(`http://ec2-18-225-9-230.us-east-2.compute.amazonaws.com/restaurants/${req.params.id}/profile`)
-//   .then(function (req) {
-//     res.status(200).send(req.data);
-//   })
-//   .catch(function (error) {
-//     res.status(501).send(error);
-//   });
+app.get('/restaurants/:id/profiles', (req, res) => {
+  // get all the profile from profile server
+  console.log('PROFILES');
+  httpReq.get(`http://52.15.232.64/restaurants/${req.params.id}/profiles`)
+  .then(function (req) {
+    res.status(200).send(req.data);
+  })
+  .catch(function (error) {
+    res.status(501).send(error);
+  });
 
-// });
+});
 
 
 app.listen(port, () => {
